590c48eff179dfa402ad02c18100d47cd8e49634
baebop <79027015+baebop@users.noreply.github.com>
2021-02-19 15:43:05 +0000

    Deploying to gh-pages from master @ 176413a4bff56cf388e60220c221668015e5e167 🚀

---
+++ person (13).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="person (13)" tilewidth="32" tileheight="32" tilecount="96" columns="48">
+ <image source="person (13).png" width="1536" height="64"/>
+</tileset>
---
+++ person (9).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="person (9)" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="person (9).png" width="128" height="64"/>
+</tileset>
